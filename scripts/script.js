16a9d7681bf9ab5f9835ac61079fa458e6c36c7a
Viren Mukhi <viren@mgglobe.com>
2019-03-22 17:06:34 +0800

    finalised gallery

--- scripts/script.js
+++ scripts/script.js
@@ -177,3 +177,20 @@ var _createClass = function () {function defineProperties(target, props) {for (v
 
 new Clock('hongkong');
 
+
+// image carousel
+$('section.awSlider .carousel').carousel({
+  pause: "hover",
+  interval: 2000
+});
+
+var startImage = $('section.awSlider .item.active > img').attr('src');
+$('section.awSlider').append('<img src="' + startImage + '">');
+
+$('section.awSlider .carousel').on('slid.bs.carousel', function () {
+ var bscn = $(this).find('.item.active > img').attr('src');
+  $('section.awSlider > img').attr('src',bscn);
+});
+
+
+
